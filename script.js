f9bc4a0d5cb6619ec5c685123c61cf34d360cdfe
vishalarmyhr32 <103406430+vishalarmyhr32@users.noreply.github.com>
2026-03-11 14:18:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+
+navMenu.classList.toggle("active");
+
+});
